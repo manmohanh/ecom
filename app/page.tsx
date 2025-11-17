39eca996f20ade0058b2d07c01a4f32c7152780b
Manmohan Hansda <hmanmohan12@gmail.com>
2025-11-17 20:24:19 +0530

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Products from "@/components/Products";
 
+export const metadata = {
+  title:`Ecom - ${process.env.DOMAIN}`,
+  description: 'India`s best and affordable ecommerce website',
+  keywords:"ecom,ecom.com,online ecommerce website"
+}
+
 const HomeRouter = async () => {
   const productRes = await fetch(`${process.env.SERVER}/api/product`);
   const products = productRes.ok
